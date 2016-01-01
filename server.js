df0c116685688d824bb5e7e1b059edb47d7c6acf
Øyvind <oyvind.nordsveen@sol.no>
2016-01-01 14:40:15 +0100

    fix startup

--- server.js
+++ server.js
@@ -7,14 +7,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-//app.get('*', function (req, res) {
-//    res.sendFile(path.join(baseName, 'public', 'index.html'));
-//});
-
-
 var server = app.listen(3000, function () {
-    var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://localhost:' + port);
 });
