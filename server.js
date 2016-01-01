82c6f94d2bdecb7c2de48ce82214d77748c64407
Øyvind <oyvind.nordsveen@sol.no>
2016-01-01 14:35:57 +0100

    move to public

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var app = express();
+
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get('*', function (req, res) {
+//    res.sendFile(path.join(baseName, 'public', 'index.html'));
+//});
+
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
